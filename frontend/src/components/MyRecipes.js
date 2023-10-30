42f7e9d1a4b991b8a38ff1ccd295f6ea5d129af7
Nikolina Milioni <59976395+nimilio@users.noreply.github.com>
2023-10-30 22:30:09 +0000

    MyRecipes component

---
+++ frontend/src/components/MyRecipes.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useNavigate  } from "react-router-dom";
+import recipeService from "../services/recipes";
+
+const MyRecipes = ({ myRecipes, setMyRecipes, user }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        recipeService
+            .getMy(user)
+            .then(allMyRecipes => {
+                setMyRecipes(allMyRecipes);
+            });
+    }, []);
+
+    return (
+        <div>
+            <p className="paragraph special">Hello {user} ! This is your personal cooking diary 📔</p>
+            <h2 className="my-recipes-title">Your Recipes</h2>
+            <ul>
+                {myRecipes.map((recipe, index) => (
+                    <li className="recipe-name" key={index}>
+                        <span
+                            style={{ cursor: "pointer" }}
+                            onClick={() => navigate(encodeURIComponent(recipe.name))}
+                        >
+                            {recipe.name}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default MyRecipes;
